601576a29c2d69c6cb08130581a208f137ec5352
Benoît Courtine <benoit.courtine@gmail.com>
2020-03-31 16:05:27 +0200

    Remove CustomWeek public constructor to forbid manual creation of invalid values, grouping structs in week module.

--- src/week.rs
+++ src/week.rs
@@ -1,6 +1,6 @@
-use crate::error::Error;
-use crate::week::custom_week::CustomWeek;
 use chrono::{Datelike, Duration, NaiveDate, Weekday};
+
+use crate::error::Error;
 
 #[derive(PartialEq, Eq, Debug, Copy, Clone)]
 pub struct WeekSpecification {
@@ -150,28 +150,130 @@ impl WeekSpecification {
     /// let french_theater_first_dow_2016_53 = NaiveDate::from_ymd(2016, 12, 28);
     /// let french_theater_last_dow_2016_53 = NaiveDate::from_ymd(2017, 1, 3);
     ///
-    /// let expected_week = CustomWeek::new(2016, 53, french_theater_first_dow_2016_53, french_theater_week.clone());
+    /// let week = french_theater_week.week(french_theater_last_dow_2016_53);
     ///
-    /// assert_eq!(expected_week, french_theater_week.week(french_theater_last_dow_2016_53));
+    /// assert_eq!(2016, week.year());
+    /// assert_eq!(53, week.week());
+    /// assert_eq!(52, week.week0());
+    /// assert_eq!(french_theater_first_dow_2016_53, week.week_start());
+    /// assert_eq!(french_theater_week, week.specification());
     /// ~~~~
     pub fn week(&self, date: NaiveDate) -> CustomWeek {
-        let year = date.year();
-        let first = self.first_day_of_week_based_year(year);
-        let last = self.last_day_of_week_based_year(year);
+        let date_year = date.year();
+        let first = self.first_day_of_week_based_year(date_year);
+        let last = self.last_day_of_week_based_year(date_year);
 
-        let (week_year, week_nb) = if date < first {
+        let (year, week) = if date < first {
             // Last week of year - 1
-            (year - 1, self.num_weeks(year - 1))
+            (date_year - 1, self.num_weeks(date_year - 1))
         } else if date > last {
             // First week of year + 1
-            (year + 1, 1)
+            (date_year + 1, 1)
         } else {
             let diff = date - first;
-            (year, 1 + (diff.num_days() as u32 / 7))
+            (date_year, 1 + (diff.num_days() as u32 / 7))
         };
 
         let week_start = date - chrono::Duration::days(self.num_days_from_first_dow(date.weekday()) as i64);
 
-        CustomWeek::new(week_year, week_nb, week_start, self.clone())
+        CustomWeek {
+            year,
+            week,
+            week_start,
+            specification: self.clone(),
+        }
+    }
+}
+
+
+/// Custom week implementation.
+///
+/// PartialOrd and Ord are not implemented, because we do not have a natural ordering for weeks with different specifications.
+/// Week start could be recalculated from year, week and specification: it is only stored by convenience for `succ`, `pred`, and `contains` functions.
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub struct CustomWeek {
+    year: i32,
+    week: u32,
+    week_start: NaiveDate,
+    specification: WeekSpecification,
+}
+
+impl CustomWeek {
+
+    pub fn year(&self) -> i32 {
+        self.year
+    }
+
+    pub fn week(&self) -> u32 {
+        self.week
+    }
+
+    /// Week number in year (0 based).
+    pub fn week0(&self) -> u32 {
+        self.week - 1
+    }
+
+    pub fn week_start(&self) -> NaiveDate {
+        self.week_start
+    }
+
+    pub fn specification(&self) -> WeekSpecification {
+        self.specification
+    }
+
+    /// The next week according to the same specification.
+    pub fn succ(&self) -> CustomWeek {
+        self.specification.week(self.week_start + Duration::weeks(1))
+    }
+
+    /// The previous week according to the same specification.
+    pub fn pred(&self) -> CustomWeek {
+        self.specification.week(self.week_start - Duration::weeks(1))
+    }
+
+    /// Verify if the given date is in the current week.
+    pub fn contains(&self, date: NaiveDate) -> bool {
+        date >= self.week_start && date < (self.week_start + Duration::weeks(1))
+    }
+
+    /// Very naive week formatting
+    ///
+    /// Formatters are inspired by `chrono::format::strftime`.
+    ///
+    /// | Spec. | Example  | Description                                            |
+    /// |-------|----------|--------------------------------------------------------|
+    /// |       |          | **DATE SPECIFIERS:**                                   |
+    /// | `%Y`  | `2001`   | The week year, zero-padded to 4 digits.                |
+    /// | `%C`  | `20`     | The week year divided by 100, zero-padded to 2 digits. |
+    /// | `%y`  | `01`     | The week year modulo 100, zero-padded to 2 digits.     |
+    /// | `%W`  | `27`     | Week number, zero-padded to 2 digits.                  |
+    /// |-------|----------|--------------------------------------------------------|
+    ///
+    /// ~~~~
+    /// use chrono::NaiveDate;
+    /// use chrono_ext::{WeekSpecification, CustomWeek};
+    ///
+    /// let french_theater_week: WeekSpecification = WeekSpecification::french_theater_week();
+    ///
+    /// let french_theater_dow_2016_53 = NaiveDate::from_ymd(2017, 1, 3);
+    /// let week = french_theater_week.week(french_theater_dow_2016_53);
+    ///
+    /// assert_eq!("Year 2016", week.format("Year %Y"));
+    /// assert_eq!("Year 2016", week.format("Year %C%y"));
+    /// assert_eq!("Week 53", week.format("Week %W"));
+    /// assert_eq!("S1653", week.format("S%y%W"));
+    /// ~~~~
+    ///
+    pub fn format(&self, fmt: &str) -> String {
+        let full_year = format!("{:04}", self.year);
+        let y_div_100 = format!("{:02}", self.year / 100);
+        let y_mod_100 = format!("{:02}", self.year % 100);
+        let week = format!("{:02}", self.week);
+
+        fmt
+            .replace("%Y", &full_year)
+            .replace("%C", &y_div_100)
+            .replace("%y", &y_mod_100)
+            .replace("%W", &week)
     }
 }
--- src/week/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod custom_week;
-pub mod week_specification;
-
-pub use custom_week::CustomWeek;
-pub use week_specification::WeekSpecification;
